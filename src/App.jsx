22d99d860e8f6eea4f9f2c64b58af664d39dd4e3
Rasel Sharif <raselsharif9@gmail.com>
2023-09-20 21:21:44 +0600

    added state ful form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import SimpleForm from "./Components/SimpleForm/SimpleForm";
+import StateFulForm from "./Components/StateFulForm/StateFulForm";
 
 function App() {
   return (
     <>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
+      <StateFulForm></StateFulForm>
     </>
   );
 }
---
+++ src/Components/StateFulForm/StateFulForm.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const StateFulForm = () => {
+  const [name, setName] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [err, setErr] = useState("");
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log(name, phone, email);
+    if (name.length < 2) {
+      setErr("plse type a atleast two character");
+    } else {
+      setErr("");
+    }
+  };
+  const nameHandle = (e) => {
+    setName(e.target.value);
+    console.log(e.target.value);
+  };
+  const phoneHandle = (e) => {
+    setPhone(e.target.value);
+    console.log(e.target.value);
+  };
+  const emailHandle = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleForm}>
+        <input onChange={nameHandle} type="text" name="name" /> <br />
+        <input onChange={phoneHandle} type="text" name="email" /> <br />
+        <input onChange={emailHandle} type="text" name="phone" /> <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <p>{err}</p>
+    </div>
+  );
+};
+
+export default StateFulForm;
